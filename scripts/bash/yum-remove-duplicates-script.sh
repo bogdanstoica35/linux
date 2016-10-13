0d93349bcd10584cd46694a2d4af553d9a933386
Bogdan Stoica <bogdan@vrem.ro>
2016-10-14 00:53:26 +0300

    Update yum-remove-duplicates-script.sh

--- scripts/bash/yum-remove-duplicates-script.sh
+++ scripts/bash/yum-remove-duplicates-script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# install yum-utils first
+# install yum-utils package
 yum -y install yum-utils
 
-# list all yum packages duplicates
+# list all duplicate packages
 package-cleanup --dupes
 
-# create a file dupes.txt with the duplicate packages
+# create a file duplicates.txt with a list of the duplicate packages
 package-cleanup --dupes > dups.txt
 
-# remove lower version of the duplicate packages from the rpm local database
+# remove lower version of the duplicate package from the rpm local database
 for i in `cat dups.txt | awk 'NR % 2 == 1'`; do rpm -e --nodeps $i; done
