2540274afb8407183831d280082d27c7f4ee6ac5
Bogdan Stoica <bogdan@vrem.ro>
2017-07-06 23:52:15 +0300

    simple bash script to change all cpanel users' passwords on a WHM server

---
+++ cpanel/change-all-cpanel-users-passwords.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+ls -1 /var/cpanel/users | while read user; do
+pass=`strings /dev/urandom | tr -dc .~?_A-Za-z0-9 | head -c16 | xargs`
+echo “$user $pass” >> new-pass.txt
+/scripts/realchpass $user $pass
+#/scripts/ftpupdate
+done
