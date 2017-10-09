6ac8ebe67a68b5e8f602b55df947d075e2ac9e4f
Bogdan Stoica <bogdan.stoica@onem.com>
2017-10-09 16:01:21 +0300

    Update change-all-cpanel-users-passwords.sh
    Update ftp password for cPanel users

--- cpanel/change-all-cpanel-users-passwords.sh
+++ cpanel/change-all-cpanel-users-passwords.sh
@@ -3,5 +3,5 @@ ls -1 /var/cpanel/users | while read user; do
 pass=`strings /dev/urandom | tr -dc .~?_A-Za-z0-9 | head -c16 | xargs`
 echo “$user $pass” >> new-pass.txt
 /scripts/realchpass $user $pass
-#/scripts/ftpupdate
+/scripts/ftpupdate # (this will update ftp passwords as well)
 done
